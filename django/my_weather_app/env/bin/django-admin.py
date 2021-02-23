2c91ec3a49c9cdba221a537cd459fab4d6974c7c
Alkin Tezuysal <alkin.tezuysal@gmail.com>
2021-02-23 16:39:17 +0300

    Clean up
    Signed-off-by: Alkin Tezuysal <alkin.tezuysal@gmail.com>

--- django/my_weather_app/env/bin/django-admin.py
+++ django/my_weather_app/env/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/askdba/Documents/GitHub/django_vitess/my_weather_app/env/bin/python3
+#!/Users/askdba/Documents/GitHub/vitess_frameworks/django/my_weather_app/env/bin/python3
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
